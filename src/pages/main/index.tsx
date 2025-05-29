c8fc48c60419a5eb1c74c1750193d1c55cfa9ffc
Alena Zykova <alena.zykova@rktv.ru>
2025-05-29 17:49:53 +0300

    feat: test

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -6,6 +6,8 @@ import { getVirtualItems } from "../../api/store.ts";
 import { VirtualItemType } from "../../api/types.ts";
 import { useAuthContext } from "../../contexts/useAuthContext";
 import VirtualItemCard from "../../components/VirtualItemCard.tsx";
+import { headlessCheckout } from "@xsolla/pay-station-sdk";
+import { buildPaymentFlow } from "./sdk-init.ts";
 
 const MainPage = () => {
   const { isAuthorized } = useAuthContext();
@@ -20,6 +22,34 @@ const MainPage = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const init = async () => {
+      await headlessCheckout.init({
+        isWebview: false,
+        sandbox: false,
+      });
+      debugger;
+      await headlessCheckout.setToken(
+        "coqu6XtWh10Ur30U9Ls1SmlGpvkUniqy_lc_en_bg_FFFFFF_tb_3D46F5",
+      );
+
+      await headlessCheckout.form.init({
+        paymentMethodId: 1380,
+        returnUrl: "http://localhost:9000/return-page",
+      });
+
+      headlessCheckout.form.onNextAction((nextAction) => {
+        switch (nextAction.type) {
+          case "check_status": {
+            console.log("showStatus =", true);
+          }
+        }
+      });
+    };
+    init();
+    // buildPaymentFlow();
+  }, []);
+
   if (!isAuthorized) {
     return (
       <Container maxWidth="sm" sx={{ paddingTop: "30px" }}>
@@ -32,6 +62,25 @@ const MainPage = () => {
 
   return (
     <>
+      <div className="application">
+        <h1>Credit card integration</h1>
+        <div className="columns-wrapper">
+          <div className="left-col">
+            {/*@ts-ignore*/}
+            <psdk-finance-details></psdk-finance-details>
+            {/*@ts-ignore*/}
+            <psdk-total></psdk-total>
+          </div>
+
+          <div className="right-col">
+            <div id="form-container"></div>
+
+            <div id="status-container"></div>
+          </div>
+        </div>
+        {/*@ts-ignore*/}
+        <psdk-legal></psdk-legal>
+      </div>
       <Typography variant="h4" align="center" paddingBottom={"30px"}>
         Virtual Items
       </Typography>
@@ -50,6 +99,28 @@ const MainPage = () => {
           </Grid>
         )}
       </Box>
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    position: "absolute",*/}
+      {/*    top: "50px",*/}
+      {/*    left: "50px",*/}
+      {/*    width: "100%",*/}
+      {/*    height: "100%",*/}
+      {/*    zIndex: 1000,*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <iframe*/}
+      {/*    src={*/}
+      {/*      // "http://localhost:4200/?token=uep0d5suCQVbhbb3Yu9tuxCN78l1sF6Z_lc_en"*/}
+      {/*      // "http://localhost:4200/?token=8sogkde49l4x81wQH6isbns7SEtYobm7_lc_en"*/}
+      {/*      // "http://localhost:4200/?token=8sogRo2MVxbdOjJztGO0HJgp1nxZC5t8_lc_en"*/}
+      {/*      "http://localhost:4200/?token=8sogYSs8R5pL5iZHjWig6Err7uksT6lF_lc_en"*/}
+      {/*    }*/}
+      {/*    width="900"*/}
+      {/*    height="1000"*/}
+      {/*    allow="clipboard-read; clipboard-write; payment"*/}
+      {/*  ></iframe>*/}
+      {/*</div>*/}
     </>
   );
 };
